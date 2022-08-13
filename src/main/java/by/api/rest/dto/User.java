89732a328bee2f28bccae1603b75c7ecc165ec07
goodr <jack.suvorov13@gmail.com>
2022-08-13 15:55:15 +0300

    HM_SUV_REST-api

---
+++ src/main/java/by/api/rest/dto/User.java
@@ -0,0 +1,20 @@
+package by.api.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class User {
+    public String name;
+    public String job;
+}
+
